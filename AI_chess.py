16d48825ccf377625a3a3428fad4e833f845061a
akherati6174 <akherati5660@gmail.com>
2020-04-30 01:36:41 +0430

    add help

--- AI_chess.py
+++ AI_chess.py
@@ -2,7 +2,11 @@ from Graphic.GUI import GUI , pygame
 from threading import Thread
      
 def main():   
-
+    print "##############   help   ##############"
+    print "# 1. pres R to reset game            #"
+    print "# 2. pres Q to quit game             #"
+    print "# 3. pres Z to undo last move        #"
+    print "######################################"
     g = GUI() 
      
 if __name__ == "__main__":
